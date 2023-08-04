e8f6932ec4cd9d8989fc89eedb6d68365ebdd063
백동흔 <128360692+beakdongH@users.noreply.github.com>
2023-08-04 19:41:29 +0900

    CodeUp-c

--- CodeUp.c
+++ CodeUp.c
@@ -1353,3 +1353,53 @@ int main(void){
 	return 0;
 }
 
+//1229(방법1)
+#include<stdio.h>
+
+int main(void){
+    double h, w, avg;
+    scanf("%lf %lf", &h, &w);
+    if(h<150) avg = h-100;
+    else if(h<160) avg = (h-150)/2+50;
+    else avg = (h-100)*0.9;
+    double per = (w-avg)*100/avg;
+    
+    if (per<=10) printf("정상");
+    else if (per<=20) printf("과체중");
+    else printf("비만");
+    
+    return 0;
+    
+    
+}
+
+//1229(방법2)
+#include<stdio.h>
+
+int main(void){
+    double h, w, per;
+    scanf("%lf %lf", &h, &w);
+    if(h<150) per = (w-(h-100))*100/(h-100);
+    else if(h<160) per = (w-((h-150)/2+50))*100/((h-150)/2+50);
+    else per = (w-((h-100)*0.9))*100/((h-100)*0.9);
+    
+    if (per<=10) printf("정상");
+    else if (per<=20) printf("과체중");
+    else printf("비만");
+    
+    return 0;
+}
+
+//1230
+#include<stdio.h>
+
+int main(void){
+	int a,b,c;
+	scanf("%d %d %d",&a,&b,&c);
+	if (a<=170) printf("CRASH %d",a);
+	else if (b<=170) printf("CRASH %d",b);
+	else if (c<=170) printf("CRASH %d",c);
+	else printf("PASS");
+	
+	return 0;
+}
