8b5877cbcb42f704950df6771477b995ac6eba1a
백동흔 <128360692+beakdongH@users.noreply.github.com>
2023-08-10 00:59:13 +0900

    CodeUp-c

--- CodeUp.c
+++ CodeUp.c
@@ -1,4 +1,82 @@
 //==========================기초 100제==========================
+//1001
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello");
+  return 0;
+}
+
+//1002
+#include<stdio.h>
+
+int main(){
+    printf("Hello World");
+    return 0;
+}
+
+//1003
+#include<stdio.h>
+
+int main(){
+    printf("Hello\nWorld");
+    return 0;
+}
+
+//1004
+#include <stdio.h>
+
+int main(){
+    printf("\'Hello\'\n");
+    return 0;
+}
+
+//1005
+#include <stdio.h>
+
+int main(){
+    printf("\"Hello World\"\n");
+    return 0;
+}
+//1006
+#include<stdio.h>
+
+int main(){
+   
+     printf("\"!@#$%%^&*()\"");
+     return 0;
+}
+
+//1007
+#include <stdio.h>
+
+int main() {
+    printf("\"C:\\Download\\hello.cpp\"");
+    return 0;
+}
+
+//1008
+#include<stdio.h>
+ 
+int main(){
+    printf("\u250c\u252c\u2510\n");
+    printf("\u251c\u253c\u2524\n");
+    printf("\u2514\u2534\u2518\n");
+    return 0;
+}
+
+//1010
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    printf("%d",n);
+    
+    return 0;
+}
+
 //1030
 #include <stdio.h>
 
