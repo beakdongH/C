c27a607ced61e5ef90d243349d9ac3f4ac8b4656
백동흔 <128360692+beakdongH@users.noreply.github.com>
2023-12-12 16:06:28 +0900

    ADD :: C

---
+++ C/1120.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void){
+    int a,b,c;
+    double sum;
+    scanf("%d %d %d",&a,&b,&c);
+    sum = (a+b+c)/3.0;
+    printf("%.2lf",sum);
+    
+    
+    return 0;
+}
