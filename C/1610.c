9e7106faef9a00c0544a8405d97b7d1ea0b66dab
백동흔 <128360692+beakdongH@users.noreply.github.com>
2023-12-26 09:09:06 +0900

    ADD :: C

---
+++ C/1610.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+char *mysubstr(char *str, int start, int count){
+	int i;
+	char sub1[1001];
+	for(i = start; i < start+count; i++)
+		sub1[i] = str[i];
+	sub1[i] = '\0';
+
+	return &sub1[start];
+}
+
+
+int main(){
+	char str[100];
+	int s,e;
+	scanf("%s", str);
+	scanf("%d%d", &s, &e);
+	printf("%s", mysubstr(str, s, e));
+	return 0;
+}
