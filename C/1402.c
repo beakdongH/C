830d2aea909dad2acd8c2f8f0a3ff2fa79a9ac83
백동흔 <128360692+beakdongH@users.noreply.github.com>
2023-12-27 11:46:51 +0900

    ADD :: C

---
+++ C/1402.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+ 
+ 
+int main(){
+    
+    int num;
+    int buf[1000] = {};
+    scanf("%d",&num);
+    
+    for(int i =0; i<num; i++){
+        scanf("%d", &buf[i]);
+    }
+  
+    for(int j = num-1; j >= 0; j--){
+        printf("%d ",buf[j]);
+    }
+ 
+    
+    return 0;
+}
