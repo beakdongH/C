a0c2635d939bfdee5bd0dd0feed366f6a217c63c
백동흔 <128360692+beakdongH@users.noreply.github.com>
2024-01-13 22:20:45 +0900

    ADD :: C

--- C/1912.c
+++ C/1912.c
@@ -1,16 +1,13 @@
-# include <stdio.h>
+#include <stdio.h>
 
-int n,m =1;
-int f(int a){
-	if(a==1) return;
-	m*=a;
-	f(a-1);
+int f(int n){
+    if (n==1) return 1;
+    return n*f(n-1);
 }
 
-
 int main(){
-	scanf("%d",&n);
-	f(n);
-	printf("%d",m);
-	return 0;
+    int n;
+    scanf("%d", &n);
+    printf("%d", f(n));
+    return 0;
 }
