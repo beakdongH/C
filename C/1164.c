ecc776028e83e9058ee0645a8fd3fd0d8e6f1f53
백동흔 <128360692+beakdongH@users.noreply.github.com>
2024-03-16 12:58:02 +0900

    ADD :: C

--- C/1164.c
+++ C/1164.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
-int main(void){
-    int n1, n2, n3; 
-    scanf("%d %d %d", &n1, &n2, &n3);
-    if (n1 > 170 && n2 > 170 && n3 > 170) printf("PASS");
-    
+int main() {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if((a>170&&b>170&&c>170)) printf("PASS");
     else printf("CRASH");
-
     return 0;
 }
-
